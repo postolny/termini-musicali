da8462fd63a3bdfa46b5f5ee9e64754988ad3c94
Igor Postolny <74405055+postolny@users.noreply.github.com>
2024-05-01 12:59:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -850,4 +850,18 @@ $(document).ready(function() {
     });
   });
 
+  // Получаем адрес текущей страницы
+  var currentPage = window.location.pathname;
+
+  // Обходим все ссылки в футере
+  $("footer a").each(function() {
+    // Получаем адрес ссылки
+    var linkHref = $(this).attr("href");
+    // Проверяем, совпадает ли адрес ссылки с адресом текущей страницы
+    if (linkHref === currentPage) {
+      // Если совпадает, скрываем ссылку и ее родительский элемент <li>
+      $(this).parent().hide();
+    }
+  });
+
 });
